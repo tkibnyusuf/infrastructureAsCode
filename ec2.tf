43b983e0f4feb106a72a916ec1bc48c2b9e1b125
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2022-09-04 15:03:59 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "ec2_security_group" {
   vpc_id      = resource.aws_default_vpc.default_vpc.id
 
   ingress {
-    description = "http access"
+    description = "new-http access"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -42,7 +42,7 @@ resource "aws_security_group" "ec2_security_group" {
   }
 
   ingress {
-    description = "ssh access"
+    description = "new-ssh access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
